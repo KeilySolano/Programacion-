6b0ef4dac1da80b4c455f2243c4e9d544f98df46
KeilySolano <62618755+KeilySolano@users.noreply.github.com>
2020-06-24 11:25:43 -0600

    Cambio02

---
+++ templates_c/470e430e2d666d53445f604645f3e545b71356a9_0.file.Default.tpl.php
@@ -0,0 +1,49 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-06-23 20:54:54
+  from 'C:\xampp\htdocs\Progra\Programacion-\templates\Default.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5ef24ffeb128b5_75541647',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '470e430e2d666d53445f604645f3e545b71356a9' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Progra\\Programacion-\\templates\\Default.tpl',
+      1 => 1592938492,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ef24ffeb128b5_75541647 (Smarty_Internal_Template $_smarty_tpl) {
+?><!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="./Framework/Bootstrap/css/bootstrap.min.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div align="Center">
+    <br>
+    <h1>Keily Solano </h1>
+   </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <link rel="stylesheet" href="./Framework/Bootstrap/js/bootstrap.min.js">
+    <link rel="stylesheet" href="./Framework/Bootstrap/js/bootstrap.bundle.min.js">
+  </body>
+</html>
+<?php }
+}
